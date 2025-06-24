0459d2297cbe1e313d066f505e003a8dade98539
Abhishek <abhishekjoshi035@.com>
2025-06-25 00:09:49 +0530

    u5

--- main.py
+++ main.py
@@ -49,15 +49,39 @@ async def chat(request: ChatRequest):
 
     # 2. Build full chat prompt
     messages = [
-        {"role": "system", "content": "You are a helpful real estate architect assistant. Use only the context provided to answer user queries. Be accurate, helpful, and detail-oriented."}
-    ]
+        {
+            "role": "system", 
+            "content": '''
+            You are a helpful and intelligent assistant. Your task is to answer user queries using the provided context when available.
+            Instructions:
+            1. If the provided context contains enough information to answer the query, use only that context to respond.
+            2. If the context is missing or does not include sufficient information, you may answer based on your own knowledge.
+            3. Clearly prioritize accuracy and relevance.
+            4. If the context is not sufficient, do not fabricate information.
+            5. You are AI with information on architecture, engineering, and construction (AEC) topics,not other field if user ask about other field then just denied poliely
+            4. Do not invent or assume details that are not in the context unless no context is provided.
+            5. Be clear, concise, and professional in all responses.
+            *System Instructions*  
+            You are a helpful assistant. Follow these rules:  
+            1. **Context Priority**:  
+            - If context is provided, use it strictly.  
+            - If context is missing/insufficient, use your knowledge only on architecture property, civil engineering, and construction (AEC) nd basic greeting topics.  
+            2. **Formatting**:  
+            - Use `\n\n` for line breaks.  
+            - Wrap **small headings** in double asterisks (`**like this**`).  
+            - Wrap *large headings* in single asterisks (`*like this*`).  
+            3. **Style**:  
 
-    for turn in chat_history[-5:]:
+                    '''}
+                          ]
+
+    for turn in chat_history[-8:]:
         messages.append({"role": "user", "content": turn["user"]})
         messages.append({"role": "assistant", "content": turn["bot"]})
 
     prompt = f"""Use the following context to answer the user's question.\n\nContext:\n{context}\nQuestion: {user_input}"""
     messages.append({"role": "user", "content": prompt})
+
 
     # 3. Call Groq LLM
     response = groq_client.chat.completions.create(
