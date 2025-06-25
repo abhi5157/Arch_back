dfaf6a14167d219dce7f20098c68702a21c84520
Abhishek <abhishekjoshi035@.com>
2025-06-25 15:52:06 +0530

    start-u2

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-source env/bin/activate
-uvicorn src.main:app --uds=/tmp/uvicorn.sock
+echo"${PWD}"
+
+source v/bin/activate
+uvicorn main:app --host=0.0.0.0  --workers=4 --port=8000 --reload
+# --uds=/tmp/uvicorn.sock
