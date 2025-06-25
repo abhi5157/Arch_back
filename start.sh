11ec3fe2eb75f8acfac8f5a2447a3ea4e7364d5c
Abhishek <abhishekjoshi035@.com>
2025-06-25 18:31:03 +0530

    u=4

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+echo"$(pwd)"
 source v/bin/activate
-uvicorn main:app --host=0.0.0.0 --port=8000 --reload --workers =4
+uvicorn main:app --host=0.0.0.0 --port=8000 --workers=4
 # Run Uvicorn (single worker mode)
